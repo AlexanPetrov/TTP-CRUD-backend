d5161d8b923dde0d1ec18ab5f14a191f1fc6b562
ruimin <ruiminxu9478@gmail.com>
2021-01-25 10:27:09 -0500

    bug in the server

--- server/database.sql
+++ server/database.sql
@@ -1,13 +1,19 @@
 CREATE DATABASE school;
 
 CREATE TABLE student (
-    student_id SERIAL PRIMARY KEY,
+    student_id INT PRIMARY KEY,
     name VARCHAR(255),
     campus_id INT,
-    FOREIGN KEY (campus_id) REFERENCES campus(campus_id)
+    FOREIGN KEY (campus_id) REFERENCES campus(campus_id),
+    gpa FLOAT,
+    studenturl VARCHAR(1000)
 );
 
 CREATE TABLE campus (
-    campus_id SERIAL PRIMARY KEY,
-    campusName VARCHAR(250)
+    campus_id INT PRIMARY KEY,
+    campusname VARCHAR(1000),
+    campuslocation VARCHAR(1000),
+    campusimageurl VARCHAR(1000),
+    campusdescription VARCHAR(1000),
+    total INT
 );
